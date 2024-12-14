5896c8030c877445d77d4c1991c9970283043f38
SPARTAN | Ray_Nar <auraka3@gmail.com>
2024-12-14 17:35:48 -0500

    Add files via upload

--- vicePresidentAutomation.py
+++ vicePresidentAutomation.py
@@ -10,11 +10,44 @@ import numpy as np
 # NOTE: if conquerors buff is enabled, scroll down before running.
 # NOTE: Update all the coordinates for your screen setup. 
 
+### Hardcoded Mouse Coordinates ###
+
+# click "list" button
+listX = 1560 
+listY = 1180
+# scroll up to top of application list
+scrollX = 1280
+scrollY = 300
+scrollY2 = 1100
+# Click Green Check Mark to Accept Application
+AcceptX = 1452
+AcceptY = 347
+# exit position card
+exitX = 1250
+exitY = 80
+# exit capitol room
+capitolExitX = 950
+capitolExitY = 1325
+# enter capitol room
+capitolEnterX = 1500
+capitolEnterY = 980
+# Scroll down to re-center screen
+recenterX = 935
+recenterY = 1000
+recenterY2 = 450
+# Click "Dismiss" button
+dismissX = 1165
+dismissY = 1195
+# Click "Confirm" dismissal button
+confirmX = 1153
+confirmY = 763
+### End of Hardcoded Mouse Coordinates ####
+
+
 # Function to convert HH:mm:ss to total minutes
 def time_to_minutes(time_str):
     hours, minutes, _ = map(int, time_str.split(':'))
     return hours * 60 + minutes
-
 
 def text_sanitization(time_str):
     if not time_str:
@@ -53,15 +86,16 @@ def remove_stale_roles(left, top, width, height, message, x, y):
         total_minutes = time_to_minutes(matches[0])
         if total_minutes >= threshold_minutes:
             print(f"{matches[0]} {message} is greater than {threshold_minutes} minutes.")
-            pyautogui.click(x, y) # click given title card
+            # click given title card
+            pyautogui.click(x, y) 
             time.sleep(.6)
-            pyautogui.click(2025, 1200) # click dismiss
+            # click dismiss
+            pyautogui.click(dismissX, dismissY) 
             time.sleep(.6)
-            pyautogui.click(2040, 760) # click Confirm
+            # click Confirm
+            pyautogui.click(confirmX, confirmY) 
             time.sleep(.6)
             # exit position card
-            exitX = 2130
-            exitY = 90
             pyautogui.click(exitX, exitY)
             time.sleep(.6)
             pyautogui.click(exitX, exitY)
@@ -72,17 +106,17 @@ def remove_stale_roles(left, top, width, height, message, x, y):
 
 def refresh_positions():
     # Click back arrow button to exit position card screen
-    pyautogui.click(1840, 1330)
+    pyautogui.click(capitolExitX, capitolExitY)
     time.sleep(1.3)
     
 	# Click back into capitol
-    pyautogui.click(2360, 980)
+    pyautogui.click(capitolEnterX, capitolEnterY)
     time.sleep(1)
     
 	# Scroll down to re-center screen
-    pyautogui.moveTo(2208, 582)
+    pyautogui.moveTo(recenterX, recenterY)
     pyautogui.mouseDown()
-    pyautogui.moveTo(2208, 330, duration=0.5)
+    pyautogui.moveTo(recenterX, recenterY2, duration=0.5)
     
 	# Release the mouse button
     pyautogui.mouseUp()
@@ -99,31 +133,25 @@ def approve_applicant_list(x, y):
     time.sleep(clickSeconds1)
     
 	# click "list" button
-    listX = 2450 
-    listY = 1170
     pyautogui.click(listX, listY)
     time.sleep(clickSeconds1)
     
 	# scrolls up twice to avoid approving players lower in the queue
-    pyautogui.moveTo(2175, 375)
+    pyautogui.moveTo(scrollX, scrollY)
     pyautogui.mouseDown()
-    pyautogui.moveTo(2175, 975, duration=0.15)
+    pyautogui.moveTo(scrollX, scrollY2, duration=0.15)
     pyautogui.mouseUp()
     time.sleep(.15)
-    pyautogui.moveTo(2175, 375)
+    pyautogui.moveTo(scrollX, scrollY)
     pyautogui.mouseDown()
-    pyautogui.moveTo(2175, 975, duration=0.18)
+    pyautogui.moveTo(scrollX, scrollY2, duration=0.18)
     pyautogui.mouseUp()
     time.sleep(.3)
-    # click approve
-    approveX = 2342
-    approveY = 350
+    # click accept application
     for i in range(3):
-        pyautogui.click(approveX, approveY)
+        pyautogui.click(AcceptX, AcceptY)
         time.sleep(clickSeconds2)
     # exit position card
-    exitX = 2130
-    exitY = 90
     pyautogui.click(exitX, exitY)
     time.sleep(clickSeconds2)
     pyautogui.click(exitX, exitY)
@@ -132,46 +160,46 @@ def approve_applicant_list(x, y):
 
 def main():
     # Conquerors Buff includes two additional position cards. Set to False if conquerors buff is disabled.
-    conquerorsBuff = False
+    conquerorsBuff = True
     
 	
 	 # coordinates is an x,y of the center of a title card.
-	 # staleRoleCoordinates is (left, top, width, height) cordinates to which this program screenshots the timer of as well as the x, y coords of the position title to click on.
+	 # staleRoleCoordinates is (left, top, width, height) coordinates to which this program screenshots the timer of as well as the x, y coords of the position title to click on.
     if conquerorsBuff:
         coordinates = [
-            (2109, 441), # Military Commander !!needs updated!! 
-            (2316, 425), # Administration Commander !!needs updated!!
-            (2175, 720), # Secretary of Strategy
-            (2400, 720), # Secretary of Security
-            (1950, 950), # Secretary of Development
-            (2175, 950), # Secretary of Science
-            (2400, 950)  # Secretary of Interior
+            (1095, 392), # Military Commander !!needs updated!! 
+            (1387, 389), # Administration Commander !!needs updated!!
+            (1260, 927), # Secretary of Strategy
+            (1487, 929), # Secretary of Security
+            (1030, 1075), # Secretary of Development
+            (1260, 1075), # Secretary of Science
+            (1490, 1075)  # Secretary of Interior
 			# Note, a player liking the bot's profile makes a permanent screen appear. This may be exitited via the "Awesome" button.
         ]
 		
         staleRoleCoordinates = [
-            (2083, 485, 77, 24, 'Military Commander', 2109, 441), #needs updated!!
-            (2293, 485, 77, 24, 'Administrative Commander', 2316, 425), #needs updated!!
-			(2135, 780, 105, 27, 'Secretary of Strategry', 2175, 720),
-            (2365, 780, 105, 27, 'Secretary of Security', 2400, 720),
-            (1910, 1075, 105, 27, 'Secretary of Development', 1950, 950),
-            (2135, 1075, 105, 27, 'Secretary of Science', 2175, 950),
-			(2365, 1075, 105, 27, 'Secretary of Interior', 2400, 950)
+            #(1090, 592, 105, 25, 'Military Commander', 1095, 392), 
+            (1354, 592, 105, 25, 'Administrative Commander', 1387, 389), 
+			(1222, 887, 105, 25, 'Secretary of Strategry', 1260, 927),
+            (1450, 887, 105, 25, 'Secretary of Security', 1487, 929),
+            (994, 1179, 105, 25, 'Secretary of Development', 1030, 1075),
+            (1222, 1179, 105, 25, 'Secretary of Science', 1260, 1075),
+			(1450, 1179, 105, 25, 'Secretary of Interior', 1490, 1075)
         ]
     else:
         coordinates = [
-            (2175, 720), # Secretary of Strategy
-            (2400, 720), # Secretary of Security
-            (1950, 950), # Secretary of Development
-            (2175, 950), # Secretary of Science
-            (2400, 950)  # Secretary of Interior	
+            (1260, 927), # Secretary of Strategy
+            (1487, 929), # Secretary of Security
+            (1030, 1075), # Secretary of Development
+            (1260, 1075), # Secretary of Science
+            (1490, 1075)  # Secretary of Interior
         ]
         staleRoleCoordinates = [
-			(2135, 780, 105, 27, 'Secretary of Strategry', 2175, 720),
-            (2365, 780, 105, 27, 'Secretary of Security', 2400, 720),
-            (1910, 1075, 105, 27, 'Secretary of Development', 1950, 950),
-            (2135, 1075, 105, 27, 'Secretary of Science', 2175, 950),
-			(2365, 1075, 105, 27, 'Secretary of Interior', 2400, 950)
+			(1222, 887, 105, 25, 'Secretary of Strategry', 1260, 927),
+            (1450, 887, 105, 25, 'Secretary of Security', 1487, 929),
+            (994, 1179, 105, 25, 'Secretary of Development', 1030, 1075),
+            (1222, 1179, 105, 25, 'Secretary of Science', 1260, 1075),
+			(1450, 1179, 105, 25, 'Secretary of Interior', 1490, 1075)
         ]
     time.sleep(5) # giving time to get screen ready
     i = 9
@@ -196,7 +224,7 @@ def main():
             print(f"script has ran {j} times")
 			
 		
-        time.sleep(4) # giving operator time to stop the script
+        time.sleep(20) # giving operator time to stop the script
 
 # Do not remove
 if __name__ == "__main__":
